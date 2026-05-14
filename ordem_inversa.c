e1bea06975094b5c6ddd93b7c634cba4eb168dec
João Gabriel Rocha da Conceição <eugabrielrochaconceicao@gmail.com>
2026-05-14 18:55:34 -0300

    Implement program to find max and min of 10 numbers
    This program reads 10 floating-point numbers, determines the largest and smallest values, and prints them.

---
+++ ordem_inversa.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (void){
+
+  float numero[10];
+  int i;
+  float maior;
+  float menor;
+  
+  for(i=9;i>=0;i--){
+
+    printf("Preenchendo a posicao %d:", i);
+    scanf("%f", &numero[i]);
+    }
+
+    maior = numero[0];
+    menor = numero[0];
+
+    for(i=0;i<10;i++)
+       if(numero[i] > maior)
+          maior = numero[i];
+       else if(numero[i] < menor)
+          menor = numero[i];
+
+  printf("O maior numero e: %.2f\n", maior);
+  printf("O menor numero e: %.2f\n", menor);
+  
+  system("pause");
+  return 0;
+}
+  
