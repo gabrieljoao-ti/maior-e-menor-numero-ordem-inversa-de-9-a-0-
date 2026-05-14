1c12259f961d7d87bb0876f9849ea470e5e5c444
João Gabriel Rocha da Conceição <eugabrielrochaconceicao@gmail.com>
2026-05-14 18:58:30 -0300

    Fix loop syntax in ordem_inversa.c

--- ordem_inversa.c
+++ ordem_inversa.c
@@ -17,12 +17,12 @@ int main (void){
     maior = numero[0];
     menor = numero[0];
 
-    for(i=0;i<10;i++)
+    for(i=0;i<10;i++){
        if(numero[i] > maior)
           maior = numero[i];
        else if(numero[i] < menor)
           menor = numero[i];
-
+    }
   printf("O maior numero e: %.2f\n", maior);
   printf("O menor numero e: %.2f\n", menor);
   
